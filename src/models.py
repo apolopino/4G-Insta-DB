893fcb6e1f0add5348e63297d0c9df04e2ce6552
apolopino <apolo.pino@gmail.com>
2021-07-07 01:25:10 +0000

    avance

--- src/models.py
+++ src/models.py
@@ -8,26 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+	__tablename__='user'
+	id = Column(Integer, primary_key=True)
+	username = Column(String(250), nullable=False, index=True)
+	firstname = Column(String(250), nullable=False)
+	lastname = Column(String(250), nullable=False)
+	email = Column(String(60), nullable=False, unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Post(Base):
+	__tablename__='post'
+	id = Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	user = relationship(User)
 
-    def to_dict(self):
-        return {}
+class Comment(Base):
+	__tablename__='comment'
+	id = Column(Integer, primary_key=True)
+	comment_text = Column(String(500))
+	author_id = Column(Integer, ForeignKey('user.id'))
+	author = relationship(User)
+	post_id = Column(Integer, ForeignKey('post.id'))
+	post = relationship(Post)
+
+class Media(Base):
+	__tablename__='media'
+	id = Column(Integer, primary_key=True)
+	type = Column(Integer)
+	url = Column(String(150))
+	post_id = Column(Integer, ForeignKey('post.id'))
+	post = relationship(Post)
+
+
+
+def to_dict(self):
+	return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
